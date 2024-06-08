d9b25f47a4dc14e9cd4f64b401ba3727f77359d8
sahilsingh-7 <164621582+sahilsingh-7@users.noreply.github.com>
2024-06-08 16:35:00 +0530

    Add files via upload

--- BST.c
+++ BST.c
@@ -7,6 +7,51 @@ typedef struct bst{
     struct bst *r;
 }node;
 
+
+node* findmin(node *root){
+    while(root->l){
+        root = root->l;
+    }
+    return root;
+}
+
+
+node* dltnode(node *root,int val){
+    node *t = root;
+
+    if(root == NULL) return root;
+    
+    else if(val < root->info) root->l = dltnode(root->l,val);
+    else if(val > root->info) root->r = dltnode(root->r,val);
+    else if(val == root->info){
+        if(!(root->l) && !(root->r)) {
+            free(root);
+            root = NULL;
+            return root;
+        }
+        else if(root->l == NULL){
+            node *temp = root->r;
+            free(root);
+            root = NULL;
+            return temp;
+        }
+        else if(root->r == NULL){
+            node *temp = root->l;
+            free(root);
+            root = NULL;
+            return temp;
+        }
+        else{
+            node *temp = findmin(root->r);
+            root->info = temp->info;
+            root->r = dltnode(root->r,temp->info);
+        }
+    }
+    return t;
+    
+}
+
+
 void inserttree(node **root,int val){
     if(*root == NULL){
         node *temp = (node*)malloc(sizeof(node));
@@ -58,7 +103,8 @@ int main(){
         printf("2 -> Traverse tree inorder\n");
         printf("3 -> Traverse tree preorder\n");
         printf("4 -> Traverse tree postorder\n");
-        printf("5 -> Exit\n\n");
+        printf("5 -> Delete node from tree\n");
+        printf("6 -> Exit\n\n");
         printf("Enter your choice\n\n");
         scanf("%d",&n);
 
@@ -81,6 +127,11 @@ int main(){
                 postorder(root);
                 break;
             case 5:
+                printf("Enter value to be Deleted : ");
+                scanf("%d",&val);
+                node *n = dltnode(root,val);
+                break;
+            case 6:
                 printf("Program ended\n");
                 return 0;
         }
